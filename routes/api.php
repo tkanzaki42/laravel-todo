f7657bd8189c4a577bf35dcfef6279d84286b5be
tkanzaki42 <tkanzaki@student.42tokyo.jp>
2023-07-06 18:54:01 +0900

    feat: エンドポイント追加

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,12 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\TodoController;
 
+Route::get('/todos', [TodoController::class, 'index']);
+Route::post('/todos', [TodoController::class, 'store']);
+Route::get('/todos/{id}', [TodoController::class, 'show']);
+Route::put('/todos/{id}', [TodoController::class, 'update']);
+Route::delete('/todos/{id}', [TodoController::class, 'destroy']);
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
